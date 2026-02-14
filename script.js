7b16db944f4bdfa6b7424d139af86984cd246867
CauaTermux <cacasaraivalopes@gmail.com>
2026-02-14 19:53:03 -0300

    Começo do sistema de lista e itens

--- script.js
+++ script.js
@@ -9,5 +9,27 @@ function salvarDados() {
     }else {
         alert('Insira as informações do personagem');
     }
-
 }
+
+function itens(nomeItem, pesoItem) {
+    let lista = document.getElementById('listaItens');
+    
+    let itemLista = document.createElement("li");
+    let spanNome = document.createElement('span');
+    let spanPeso = document.createElement('span');
+    
+    lista.appendChild(itemLista);
+    itemLista.appendChild(spanNome);
+    spanNome.innerText = nomeItem;
+    itemLista.appendChild(spanPeso);
+    spanPeso.innerText = pesoItem;
+}
+
+document.getElementById('botaoIncluirItem').addEventListener("click", function() {
+    let nomeItem = document.getElementById('textNameItem').value
+    let pesoItem = document.getElementById('numberPesoItem').value
+    
+    
+    
+    itens(nomeItem, pesoItem);
+});
