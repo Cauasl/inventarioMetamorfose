4e62d90d253db27b6e5e64813e801232704cc132
Cauã S. Lopes <cacasaraivalopes@gmail.com>
2026-02-16 00:23:06 -0300

    Card e controle de vida do personagem adicionados

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function apagarDados() {
     localStorage.removeItem('inventarioJogador');
     localStorage.removeItem('pesoMochila');
     localStorage.removeItem('nome');
-    localStorage.removeItem('vida');
-    //localStorage.removeItem('');
+    localStorage.removeItem('vida-max');
+    localStorage.removeItem('vidaAtual');
 }
 
 //Coloca o peso da mochila na tela
 let pesoTotal = document.getElementById('peso');
 let numID = -1;
+let vidaAtual = Number(localStorage.getItem('vidaAtual'));
 
 let pesoMochila = Number(localStorage.getItem('pesoMochila'));
 let itensInventario = JSON.parse(localStorage.getItem('inventarioJogador'));
@@ -17,7 +18,7 @@ console.log(itensInventario);
 
 function dadosExistente() { //Verifica se exite informações sobre o personagem no navegador
     let nomePerssona = localStorage.getItem('nome');
-    let vidaerssona = localStorage.getItem('vida');
+    let vidaerssona = localStorage.getItem('vida-max');
     
     if(nomePerssona != null && vidaerssona != null) {
         console.log('foi');
@@ -34,14 +35,17 @@ function salvarDados() { //Salva os dados do usúario e inicia as outras informa
 
     if(nome != '' && vida != null && !dadosExistente()) {
         localStorage.setItem('nome', nome);
-        localStorage.setItem('vida', vida);
+        localStorage.setItem('vida-max', vida);
         localStorage.setItem('inventarioJogador', JSON.stringify([])); //Cria o armazenamento da mochila
             itensInventario = JSON.parse(localStorage.getItem('inventarioJogador'));
         localStorage.setItem('pesoMochila', 0); //Cria o peso da mochila
         console.log('Dados foram salvos');
 
         document.getElementById('subtituloNome').innerText = nome;
-        document.getElementById('infoVida').innerText = vida + ' de vida';
+
+        vidaAtual = vida;
+        localStorage.setItem('vidaAtual', vida);
+        document.getElementById('infoVida').innerText = vidaAtual + ' de vida';
         
         //Apagando formulario
         document.getElementById('corpo').removeChild(document.getElementById('formulario'));
@@ -79,7 +83,8 @@ document.getElementById('botaoIncluirItem').addEventListener("click", function()
     let res;
 
     if(nomeItem != '' && pesoItem != '' && dadosExistente()) { //Só adiciona o item se os inputs não estiverem vazios
-        
+
+        controleVidaPersonagem(); //Adiciona função aos botões de controle de vida do personagem
         
         //Adiciona o peso do item a mochila e atualiza a informação no navegador
         pesoMochila += pesoItem;
@@ -118,12 +123,41 @@ function removerItem() {
     }
 }
 
+//Adiciona função aos botões de controle de vida do personagem
+function controleVidaPersonagem() {
+    const botaoControle = document.getElementsByName('botaoControle');
+    const vidaMax = Number(localStorage.getItem('vida-max'));
+    let val;
+    for(let i=0; i < botaoControle.length; i++) {
+        botaoControle[i].addEventListener('click', () => {
+            val = Number(botaoControle[i].innerText);
+            if(val > 0) {
+                vidaAtual += val;
+                if(vidaAtual > vidaMax) {
+                    vidaAtual = vidaMax;
+                }
+                localStorage.setItem('vidaAtual', vidaAtual);
+                document.getElementById('infoVida').innerText = vidaAtual + " de vida";
+            }else {
+                vidaAtual += val;
+                if(vidaAtual < 0) {
+                    vidaAtual = 0;
+                }
+                document.getElementById('infoVida').innerText = vidaAtual + " de vida";
+                localStorage.setItem('vidaAtual', vidaAtual);
+            }
+        });
+    }
+}
 
 if(itensInventario != null && dadosExistente()) { //Coloca as informações no site
+    //Coloca os itens do inventario na tela
     for(let i=0; i < itensInventario.length; i++) {
         itens(itensInventario[i].nome, itensInventario[i].peso, itensInventario[i].id)
     }
     document.getElementById('peso').innerText = "Peso: " + pesoMochila + 'kg';
+
+    controleVidaPersonagem();
 
     //Informação vizual do peso da mochila
     if(pesoMochila == 20) {
@@ -133,7 +167,12 @@ if(itensInventario != null && dadosExistente()) { //Coloca as informações no s
     }
 
     document.getElementById('subtituloNome').innerText = localStorage.getItem('nome');
-    document.getElementById('infoVida').innerText = localStorage.getItem('vida') + ' de vida';
+    if(localStorage.getItem('vidaAtual') == null) {
+        document.getElementById('infoVida').innerText = localStorage.getItem('vida-max') + ' de vida';
+    }else {
+        document.getElementById('infoVida').innerText = localStorage.getItem('vidaAtual') + ' de vida';
+    }
+    
     
     //Apagando formulario
     document.getElementById('corpo').removeChild(document.getElementById('formulario'));
